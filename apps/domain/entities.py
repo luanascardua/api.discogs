8a3f7ad5d731b7aeecbb3a09daadb281cd2d3767
luanascardua <luanascardua7@gmail.com>
2025-01-08 11:41:15 -0300

    initial commit

---
+++ apps/domain/entities.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+
+
+class Track:
+    def __init__(self, number: int, title: str, duration: str):
+        self.number = number
+        self.title = title
+        self.duration = duration
+
+    def to_dict(self):
+        return {
+            "number": self.number,
+            "title": self.title,
+            "duration": self.duration,
+        }
+
+
+class Album:
+    def __init__(self, name: str, year: Optional[int], label: str, styles: List[str], tracks: List[Track]):
+        self.name = name
+        self.year = year
+        self.label = label
+        self.styles = styles
+        self.tracks = tracks
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "year": self.year,
+            "label": self.label,
+            "styles": self.styles,
+            "tracks": [track.to_dict() for track in self.tracks],
+        }
+
+
+class Artist:
+    def __init__(self, name: str, genre: str, members: List[str], websites: List[str], albums: List[Album]):
+        self.name = name
+        self.genre = genre
+        self.members = members
+        self.websites = websites
+        self.albums = albums
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "genre": self.genre,
+            "members": self.members,
+            "websites": self.websites,
+            "albums": [album.to_dict() for album in self.albums],
+        }
---
+++ apps/domain/service.py
@@ -0,0 +1,21 @@
+from apps.adapters.discogs_api import DiscogsAPI
+from apps.adapters.file_storage import FileStorage
+
+
+class ArtistService:
+    def __init__(self, discogs_api: DiscogsAPI, file_storage: FileStorage):
+        self.discogs_api = discogs_api
+        self.file_storage = file_storage
+
+    def fetch_and_save_artist_data(self, genre: str, filename: str):
+        artists = self.discogs_api.search_artists_by_release_genre("rock")
+        data = []
+
+        for artist_name in artists:
+            albums = self.discogs_api.get_album_details(artist_name)
+            artist_resource_url, artist_id = self.discogs_api.search_artist(artist_name)
+            artist_details = self.discogs_api.get_artist_details(artist_resource_url, albums)
+            if artist_details:
+                data.append(artist_details.to_dict())
+
+        self.file_storage.save_to_json(filename, data)
